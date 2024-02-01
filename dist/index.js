b0c4472ed943ba1a8597647ebed90179c0fd4b7f
Pervez Nagi <pervezismailnagi@gmail.com>
2024-02-01 16:20:09 +0300

    Created a 3rd build

--- dist/index.js
+++ dist/index.js
@@ -8,16 +8,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const users_1 = __importDefault(require("./routes/users"));
 const goals_1 = __importDefault(require("./routes/goals"));
 const savings_1 = __importDefault(require("./routes/savings"));
-const views_1 = __importDefault(require("./routes/views"));
 const body_parser_1 = __importDefault(require("body-parser")); // For parsing incoming request bodies
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.set('view engine', 'ejs');
-app.set('views', '/views');
-app.use(express_1.default.static('public'));
 app.use(body_parser_1.default.json());
-app.use('/', views_1.default);
 app.use('/users', users_1.default);
 app.use('/goals', goals_1.default);
 app.use('/savings', savings_1.default);
