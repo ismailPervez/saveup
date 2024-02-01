b842e3261dda9ad8b3098f96691f2fd52b72a9e1
Pervez Nagi <pervezismailnagi@gmail.com>
2024-02-01 16:14:56 +0300

    Removed the views

---
+++ readme.md
@@ -0,0 +1,4 @@
+# SaveUp - Savings Management
+
+This system is designed using NodeJS and the Express framework for the api while the front-end was developed in html (ejs templating framework), css, bootstrap CSS and js.
+
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import userRouter from './routes/users';
 import goalRouter from './routes/goals';
 import savingRouter from './routes/savings';
-import viewRouter from './routes/views';
 import bodyParser from 'body-parser'; // For parsing incoming request bodies
 
 dotenv.config();
@@ -11,13 +10,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', '/views');
-
-app.use(express.static('public'));
-
 app.use(bodyParser.json());
-app.use('/', viewRouter);
 app.use('/users', userRouter);
 app.use('/goals', goalRouter);
 app.use('/savings', savingRouter);
